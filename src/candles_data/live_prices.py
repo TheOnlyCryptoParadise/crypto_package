ef8112d32a1fb09d0ecaf326cae749cc78d9e755
Natalia <natib13@gmail.com>
2021-10-16 01:18:23 +0200

    live prices connects to server

--- src/candles_data/live_prices.py
+++ src/candles_data/live_prices.py
@@ -1,7 +1,9 @@
+import pandas
+
 import service_config as conf
 import requests
 
-def get_live_price(exchange: str, currency_pairs: [str]):
+def get_live_price_full(exchange: str, currency_pairs: [str]):
     """Gets prices of defined currencies.
 
         Parameters:
@@ -11,15 +13,17 @@ def get_live_price(exchange: str, currency_pairs: [str]):
                 list of strings performing name of the pair (eg. UDT/STH)
     """
     args = _make_get_price_args(exchange, currency_pairs)
-    res = requests.post(conf.CANDLE_DATA_SERVICE + conf.EP_LIVE_PRICES, args)
+    res = requests.post(conf.CANDLE_DATA_SERVICE + conf.EP_LIVE_PRICES, json=args)
 
     if res.status_code != 200:
         raise Exception("Some exception occurred while connecting to server."+str(res))
-    print(res.text)
+    res = res.json()['data'][0]
+    print(pandas.DataFrame(res.json()['data'][0]))
     # price = [x for x.json() in res.json()
     return res
 
-
+def get_live_price(exchange: str, currency_pairs: [str]):
+    full = get_live_price_full(exchange,currency_pairs)
 
 def _make_get_price_args(exchange, pairs):
     args = {"exchanges": [
@@ -33,4 +37,4 @@ def _make_get_price_args(exchange, pairs):
     return args
 
 if __name__ == '__main__':
-    get_live_price('binance', ['BTC/USDT','BTC/USDT'])
+    get_live_price_full('binance', ['BTC/USDT','DOT/USDT'])
