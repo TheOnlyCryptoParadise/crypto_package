9038268982cb587e3c5855248202462ea1ef8455
lewelyn7 <karol.hamielec@gmail.com>
2021-11-05 00:00:13 +0100

    refactor

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cryptoparadise-lib",
+    name="cryptoparadise_lib",
     version="0.0.1",
     author="Natalia Brzozowska",
     author_email="author@example.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    # package_dir={"": "src"},
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
