438702a18e59efb4d4811a169dfbe678c4f8e92e
lewelyn7 <karol.hamielec@gmail.com>
2021-11-04 23:35:35 +0100

    add environ.get to config

--- src/candles_data/config.py
+++ src/candles_data/config.py
@@ -1,5 +1,5 @@
 """Candle Data Servide address"""
-
-CANDLE_DATA_SERVICE = 'adres ip'
+from os import environ
+CANDLE_DATA_SERVICE = environ.get("CANDLE_DATA_SERVICE_URL", "http://localhost:5000")
 EP_CANDLES = '/candles'
 EP_SETTINGS = '/settings'
