f2d340b38ef8e1aacfe8795022e0d61def96e1c7
Karol Hamielec <52937613+lewelyn7@users.noreply.github.com>
2021-11-05 12:33:25 +0100

    Update service_config.py

--- crypto_package/conf/service_config.py
+++ crypto_package/conf/service_config.py
@@ -3,3 +3,4 @@ from os import environ
 CANDLE_DATA_SERVICE = environ.get("CANDLE_DATA_SERVICE_URL", "http://localhost:5000")
 EP_CANDLES = '/candles'
 EP_SETTINGS = '/settings'
+EP_LIVE_PRICES = '/currencyPairLiveInfo'
