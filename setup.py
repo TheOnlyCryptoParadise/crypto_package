5826d19a0380be8876920a5355abba450471bcce
Karol Hamielec <52937613+lewelyn7@users.noreply.github.com>
2021-12-13 21:24:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,21 @@ from pip._internal.req import parse_requirements
 reqs = [
     "requests~=2.25.1",
     "setuptools~=49.2.1",
-    "pandas"
+    "pandas",
+    "boto3==1.20.5",
+    "grpcio==1.41.0",
+    "mariadb==1.0.8",
+    "pandas==1.3.4",
+    "pika==1.2.0",
+    "protobuf==3.19.1",
+    "pydantic==1.8.2",
+    "python-dotenv==0.19.2",
+    "PyYAML==6.0",
+    "requests==2.25.1",
+    "setuptools==49.2.1",
+    "TA_Lib==0.4.21",
+    "technical==1.3.0",
+    "tenacity==8.0.1"
 ]
 setuptools.setup(
     name="crypto_package",
